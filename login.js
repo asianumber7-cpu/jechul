4248d5de778ae615abafa1d77a520fc58f17a13b
asianumber7-cpu <asianumber7@gmail.com>
2025-08-21 10:35:28 +0900

    login success

---
+++ login.js
@@ -0,0 +1,16 @@
+
+
+const login = () => {
+
+    const click1 =()=>{
+        alert('Login Success')
+    }
+
+    return (
+        <div>
+            <button onClick={click1}>로그인</button>
+        </div>
+    );
+};
+
+export default login;
